6196770ea0f455bb48d8162d2ac3ba6972c7cbc5
zhoulin <zhoulin@xiangrong.pro>
2023-06-27 10:19:08 +0800

    完善 header 部分

--- layouts/base/header.vue
+++ layouts/base/header.vue
@@ -1,6 +1,7 @@
 <template>
     <header class="w-full shadow-sm">
-        <div class="container flex items-center h-[72px]">
+        <!-- 大点屏幕布局 -->
+        <div class="container md:flex hidden items-center h-[72px]">
             <img class="w-[72px] h-[72px] mr-10" src="../../assets/image/base/logo.png" alt="首页" />
 
             <a v-for="(item, index) in links" :key="index" class="flex items-center relative group cursor-pointer h-full mr-5" :href="item.value">
@@ -11,7 +12,33 @@
             <a class="text-gray-600 text-[13px] cursor-pointer ml-auto hover:text-black" href="#">登录</a>
             <a class="text-gray-600 text-[13px] cursor-pointer hover:text-black ml-3" href="#">注册</a>
         </div>
+
+        <!-- 小屏幕下的布局 -->
+        <div class="container md:hidden h-[60px] flex items-center relative">
+            <img class="w-[60px] h-[60px] mr-10" src="../../assets/image/base/logo.png" alt="首页" />
+
+            <span class="ml-auto w-8 h-8 text-gray-900">
+                <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"></path>
+                </svg>
+            </span>
+
+            <div class="w-screen h-screen bg-black/40 absolute left-0 top-0">
+                <div class="w-60 h-screen bg-white flex flex-col p-4 absolute top-0 right-0 rounded-s-sm">
+                    <a v-for="(item, index) in links" :key="index" class="flex items-center relative group cursor-pointer w-full h-12 mr-5" :href="item.value">
+                        <div class="text-black text-[15px] font-medium">{{ item.label }}</div>
+                        <div :class="lineClass(item)"></div>
+                    </a>
+                    
+                    <div class="mt-5">
+                        <a class="text-gray-600 text-[13px] cursor-pointer hover:text-black" href="#">登录</a>
+                        <a class="text-gray-600 text-[13px] cursor-pointer hover:text-black ml-3" href="#">注册</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
+
 </template>
 
 <script setup lang="ts">

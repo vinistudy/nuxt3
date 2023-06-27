cfe0e0bd22943b4cb4228f78e539adedcece1295
zhoulin <zhoulin@xiangrong.pro>
2023-06-27 09:18:18 +0800

    footer 完善

--- layouts/base/footer.vue
+++ layouts/base/footer.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="container">
-    <h2>我是底部</h2>
-  </div>
+  <footer class="w-f w-full bg-black">
+    <div class="md:flex container">
+      <ul class="md:flex hidden flex-1 flex-col items-center md:items-start py-5">
+        <li class="text-white font-semibold text-base">友情链接</li>
+        <li class="text-gray-300 text-sm hover:text-white mt-3"><a href="#">贼盘网</a></li>
+        <li class="text-gray-300 text-sm hover:text-white mt-2"><a href="#">约拍宝</a></li>
+        <li class="text-gray-300 text-sm hover:text-white mt-2"><a href="#">非你网</a></li>
+      </ul>
+
+      <ul class="flex flex-1 flex-col items-center md:items-start py-5">
+        <li class="text-white font-semibold text-base">联系与合作</li>
+        <li class="text-gray-300 text-sm hover:text-white mt-3"><a href="#">电话：136xxxxxxxx</a></li>
+      </ul>
+
+      <ul class="flex flex-1 flex-col items-center md:items-start py-5">
+        <li class="text-white font-semibold text-base">关注我们</li>
+        
+        <img class="w-40 h-40 rounded-lg mt-3" src="../../assets/image/base/fuwuhao.jpg" alt="公众号"/>
+      </ul>
+    </div>
+
+    <div class="text-gray-400 text-sm text-center my-4 hover:text-white mt-2">
+      <a href="#">沪ICP备19010902号</a>
+    </div>
+  </footer>
 </template>
